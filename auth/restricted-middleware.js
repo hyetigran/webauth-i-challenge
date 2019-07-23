a9b6de905c369d10a4a6fff3eeb44f1236e2c20b
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-23 18:15:58 +0400

    add modification to restricted middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,20 @@
-const bcrypt = require('bcryptjs')
-const Users = require('../models/user-model')
-
 module.exports = function restricted (req, res, next) {
-  const {username, password} = req.headers
-
-  if (username && password ) {
-      //console.log(req.body)
-      Users.findBy(username).first().then(user => {
-        //console.log(user)
-        if (user && bcrypt.compareSync(password, user.password)){
-          //console.log('you are in the success')
-          next()
-        } else {
-          //console.log('you are in the invalid')
-          res.status(401).json({ message: 'Invalid Credentials'});
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error)
-      })
+  if (req.session && req.session.user ) {
+      // //console.log(req.body)
+      // Users.findBy(username).first().then(user => {
+      //   //console.log(user)
+      //   if (user && bcrypt.compareSync(password, user.password)){
+      //     //console.log('you are in the success')
+      //     next()
+      //   } else {
+      //     //console.log('you are in the invalid')
+      //     res.status(401).json({ message: 'Invalid Credentials'});
+      //   }
+      // })
+      // .catch(error => {
+      //   res.status(500).json(error)
+      // })
+      next()
   } else {
     res.status(401).json( {message: "Please provide valid credentials"})
   }
